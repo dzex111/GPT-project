b9602d9ae9f3ed399c3e9d47432b4bf3679f6104
dzex111 <kinezedge@gmail.com>
2026-09-19 23:34:23 +0200

    Add HTTP error types

---
+++ src/http/errors.ts
@@ -0,0 +1,29 @@
+export class AppError extends Error {
+  readonly statusCode: number;
+  readonly code: string;
+
+  constructor(statusCode: number, code: string, message: string) {
+    super(message);
+    this.name = "AppError";
+    this.statusCode = statusCode;
+    this.code = code;
+  }
+}
+
+export class ValidationError extends AppError {
+  constructor(message: string) {
+    super(400, "VALIDATION_ERROR", message);
+  }
+}
+
+export class UnauthorizedError extends AppError {
+  constructor(message = "Unauthorized") {
+    super(401, "UNAUTHORIZED", message);
+  }
+}
+
+export class ConflictError extends AppError {
+  constructor(message = "Concurrent state update detected") {
+    super(409, "CONFLICT", message);
+  }
+}
