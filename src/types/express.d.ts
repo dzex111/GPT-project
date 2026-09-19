c30bd95e92b741325a0597d35ced6db1c9ba0398
dzex111 <kinezedge@gmail.com>
2026-09-19 23:44:28 +0200

    Add typed admin request context

--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -1,7 +1,14 @@
 import "express-serve-static-core";
 
+export type AdminPrincipal = {
+  role: "admin";
+  tenantId?: string;
+  subject?: string;
+};
+
 declare module "express-serve-static-core" {
   interface Request {
     rawBody?: Buffer;
+    admin?: AdminPrincipal;
   }
 }
